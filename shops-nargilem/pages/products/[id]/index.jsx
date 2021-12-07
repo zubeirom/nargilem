014e8a85c379beb6e860e7a525cb1a1668946074
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-12-08 00:27:41 +0100

    fix properteis

--- shops-nargilem/pages/products/[id]/index.jsx
+++ shops-nargilem/pages/products/[id]/index.jsx
@@ -60,9 +60,9 @@ const product = {
     },
   ],
   colors: [
-    { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
-    { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
-    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+    { name: 'White', className: 'bg-white', selectedClass: 'ring-gray-400' },
+    { name: 'Gray', className: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+    { name: 'Black', className: 'bg-gray-900', selectedClass: 'ring-gray-900' },
   ],
   sizes: [
     // { name: 'XXS', inStock: false },
@@ -255,15 +255,15 @@ export default function Product() {
 
             <form className="mt-10">
               {/* Quantity */}
-              <div class="inline-block relative w-full">
-                <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+              <div className="inline-block relative w-full">
+                <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                   {Array.from(Array(101).keys()).map((i) => {
                     if (i !== 0) return <option>{i}</option>
                   })}
                 </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                   {/* <svg
-                    class="fill-current h-4 w-4"
+                    className="fill-current h-4 w-4"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
@@ -273,29 +273,29 @@ export default function Product() {
               </div>
               {/* FEATURES */}
               <div
-                class="bg-white border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md mt-10"
+                className="bg-white border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md mt-10"
                 role="alert"
               >
-                <div class="flex">
-                  <div class="py-1">
+                <div className="flex">
+                  <div className="py-1">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6 mr-4"
+                      className="h-6 w-6 mr-4"
                       fill="none"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
                       />
                     </svg>
                   </div>
                   <div>
-                    <p class="font-bold">Jetzt sparen</p>
-                    <p class="text-sm">Kaufe mehr als 10 und spare 30%!</p>
+                    <p className="font-bold">Jetzt sparen</p>
+                    <p className="text-sm">Kaufe mehr als 10 und spare 30%!</p>
                   </div>
                 </div>
               </div>
